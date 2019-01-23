09692d75e07b46a5d7f14915186a10f847a8153c
Felipe Pudo <felipepudo@intranetconnections.com>
2019-01-22 16:10:28 -0800

    adding gitignore

--- urlPreview/controller.js
+++ urlPreview/controller.js
@@ -7,12 +7,6 @@ module.exports.getPreview = (req, res, next) => {
     rp(url).then((html) => {
         const $ = cheerio.load(html);
         res.status(200);
-        // const metas = [];
-        // $('meta').each((i, el) => {
-        //     if ('property' in el.attribs) {
-        //         metas.push({ [el.attribs.property]:  el.attribs.content })
-        //     }
-        // })
         req.contentReturn = {
             "title": $('meta[property="og:title"]').attr('content'),
             "image": $('meta[property="og:image"]').attr('content'),
